14fb2062d78ddd410da07f2c006b9a7f99427f0e
BrennerFerreira <brennercsferreira@gmail.com>
2021-08-20 10:12:00 -0300

    fix: correct position generator to avoid tiles 1 and 100

--- lib/entities/position_generator/position_generator.dart
+++ lib/entities/position_generator/position_generator.dart
@@ -3,12 +3,6 @@ import 'dart:math';
 class PositionGenerator {
   static int generatePosition() {
     final random = Random();
-    final number = random.nextInt(100);
-
-    if (number == 0) {
-      return generatePosition();
-    }
-
-    return number;
+    return random.nextInt(97) + 2;
   }
 }
